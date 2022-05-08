b2c38e2ab57318c7becd899940b3a5006371a312
KeumDongWoo <seletr96@gmail.com>
2022-05-08 20:15:31 +0900

    정률 할인정책 추가 / 관심사 분리

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,8 +11,9 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp  {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         // 회원 생성
         Long memberId = 1L;
